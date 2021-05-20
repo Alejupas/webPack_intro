3fe0d4a5c76b35c8cd3c50f9262f2435d16a8fdd
Mindaugas Kapickas <mindaugas@mkp.com>
2021-05-20 11:32:56 +0300

    entry & output config

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,15 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-  entry: {},
-  output: {},
+  entry: {
+    //nurodom musu programos pagrindini js faila
+    //ivesties laukas
+    main: path.resolve(__dirname, "./src/app.js"),
+  },
+  output: {
+    // isvesties laukas - is entry bus paimta ir ideta i output
+    filename: "main.bundle.js",
+    path: path.resolve(__dirname, "./dist"),
+  },
   plugins: [],
 };
