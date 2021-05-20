67d4af1376ed5b1810cccbd1b6e7db333d77fe02
Mindaugas Kapickas <mindaugas@mkp.com>
2021-05-20 11:09:28 +0300

    webpack config

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,1 @@
-(() => {
-  "use strict";
-  console.log("add(2, 3, 5) =", 10),
-    console.log("say(kazkas):", "I am saying " + "everything how");
-})();
+(()=>{"use strict";console.log("add(2, 3, 5) =",10),console.log("say(kazkas):","I am saying "+"everything how")})();
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: {},
+  output: {},
+  plugins: [],
+};
