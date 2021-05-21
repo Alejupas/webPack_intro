78ca97fde3c2f03ef27f5875d73bc352816bfd39
Mindaugas Kapickas <mindaugas@mkp.com>
2021-05-21 08:09:01 +0300

    upd

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -1,1 +1,6 @@
-(()=>{"use strict";console.log("add(2, 3, 5) =",10),console.log("say(kazkas):","I am saying "+"everything how");console.log("copy of obj:",{age:30,name:"Bob",gender:"male"})})();
+(() => {
+  "use strict";
+  console.log("add(2, 3, 5) =", 10),
+    console.log("say(kazkas):", "I am saying " + "everything how");
+  console.log("copy of obj:", { age: 30, name: "Bob", gender: "male" });
+})();
